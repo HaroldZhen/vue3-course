75beb4f5407fb0ac9ba0bee84aea24815af9f354
harold jhan <haroldzhen531@gmail.com>
2021-06-17 23:01:13 +0800

    update env

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? process.env.PUBLIC_PATH : '/',
+  publicPath: process.env.VUE_APP_NODE_ENV === 'production' ? process.env.VUE_APP_PUBLIC_PATH : '/',
 };
